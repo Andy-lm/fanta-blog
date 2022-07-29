1b845d320c2204007a6a0b767c31cd6ffb83fb6b
Andy <njloveqd@foxmail.com>
2022-07-29 08:56:29 +0800

    1. 抽离useForm组件 2. 使用useForm组件封装newPost

---
+++ hooks/useForm.tsx
@@ -0,0 +1,83 @@
+import { FormEventHandler, ReactChild, useCallback, useState } from "react";
+
+type useFormOptions<T> = {
+  initFormData: T;
+  fields: Field<T>[];
+  onSubmit: (formData: T) => void;
+  buttons: ReactChild;
+};
+
+type Field<T> = {
+  label: string;
+  type: "text" | "password" | "textarea";
+  key: keyof T;
+};
+
+// 这里用泛型的目的是每个组件接收的initFormData的类型都是不一样的
+function useForm<T>(options: useFormOptions<T>) {
+  const { initFormData, fields, onSubmit, buttons } = options;
+  const [formData, setFormData] = useState({
+    ...initFormData,
+  });
+  const [errors, setErrors] = useState(() => {
+    const obj: { [k in keyof T]?: string[] } = {};
+    for (let key in initFormData) {
+      if (initFormData.hasOwnProperty(key)) {
+        obj[key] = [];
+      }
+    }
+    return obj;
+  });
+
+  /**
+   * 处理表单数据变动
+   */
+  const onChange = useCallback(
+    (type, e) => {
+      setFormData({ ...formData, [type]: e.target.value });
+    },
+    [formData]
+  );
+
+  const _onSubmit: FormEventHandler = (e) => {
+    e.preventDefault();
+    onSubmit(formData);
+  };
+
+  const form = (
+    <form onSubmit={_onSubmit}>
+      {fields.map((field) => {
+        return (
+          <div key={field.key.toString()}>
+            <label>
+              {field.label}
+              {field.type === "textarea" ? (
+                <textarea
+                  onChange={(e) => {
+                    onChange("content", e);
+                  }}
+                  value={formData[field.key].toString()}
+                />
+              ) : (
+                <input
+                  type={field.type}
+                  value={formData[field.key].toString()}
+                  onChange={(e) => {
+                    onChange("title", e);
+                  }}
+                />
+              )}
+            </label>
+            {errors[field.key]?.length > 0 && (
+              <div>{errors[field.key].join(",")}</div>
+            )}
+          </div>
+        );
+      })}
+      {buttons}
+    </form>
+  );
+  return { form, setErrors };
+}
+
+export default useForm;
--- pages/posts/new_post.tsx
+++ pages/posts/new_post.tsx
@@ -1,87 +1,133 @@
 import Axios, { AxiosResponse } from "axios";
 import Form from "components/Form";
+import useForm from "hooks/useForm";
 import { NextPage } from "next";
 import { ChangeEventHandler, useCallback, useMemo, useState } from "react";
 
-const NewPosts: NextPage = (props) => {
-  const [formData, setFormData] = useState({
+const NewPosts: NextPage = () => {
+  const initFormData = {
     title: "",
     content: "",
-  });
+  };
 
-  const [errors, setErrors] = useState({
-    title: [],
-    content: [],
-  });
+  const onSubmit = (formData: typeof initFormData) => {
+    Axios.post("/api/v1/posts", formData).then(
+      (response) => {
+        console.log(response.data, "-------");
+      },
+      (error) => {
+        const response: AxiosResponse = error.response;
+        if (response.status === 422) {
+          console.log("response.data");
+          console.log(response.data);
+          setErrors({ ...response.data });
+        }
+      }
+    );
+  };
 
-  /**
-   * Form组件的fields
-   */
-  const formFields: FormFields[] = useMemo(() => {
-    return [
+  const buttons = (
+    <div>
+      <button type="submit">提交</button>
+    </div>
+  );
+
+  const { form, setErrors } = useForm({
+    initFormData,
+    fields: [
       {
         label: "文章标题",
         type: "text",
-        value: formData.title,
-        onChange: (e: ChangeEventHandler) => {
-          onChange("title", e);
-        },
-        errors: errors.title,
+        key: "title",
       },
       {
         label: "内容",
         type: "textarea",
-        value: formData.content,
-        onChange: (e: ChangeEventHandler) => {
-          onChange("content", e);
-        },
-        errors: errors.content,
+        key: "content",
       },
-    ];
-  }, [formData, errors]);
+    ],
+    onSubmit,
+    buttons,
+  });
 
-  /**
-   * 处理表单数据变动
-   */
-  const onChange = useCallback(
-    (type, e) => {
-      setFormData({ ...formData, [type]: e.target.value });
-    },
-    [formData]
-  );
+  // const [formData, setFormData] = useState({
+  //   title: "",
+  //   content: "",
+  // });
 
-  /**
-   * 处理数据提交
-   */
-  const onSubmit = useCallback(
-    (e) => {
-      e.preventDefault();
-      // 发送请求前将上一次错误置空
-      setErrors({
-        title: [],
-        content: [],
-      });
-      Axios.post("/api/v1/poste", formData).then(
-        () => {
-          alert("发布成功！");
-        },
-        (error) => {
-          const response: AxiosResponse = error.response;
-          if (response.status === 422) {
-            console.log("response.data");
-            console.log(response.data);
-            setErrors({ ...response.data });
-          }
-        }
-      );
-    },
-    [formData]
-  );
+  // const [errors, setErrors] = useState({
+  //   title: [],
+  //   content: [],
+  // });
+
+  // /**
+  //  * Form组件的fields
+  //  */
+  // const formFields: FormFields[] = useMemo(() => {
+  //   return [
+  //     {
+  //       label: "文章标题",
+  //       type: "text",
+  //       value: formData.title,
+  //       onChange: (e: ChangeEventHandler) => {
+  //         onChange("title", e);
+  //       },
+  //       errors: errors.title,
+  //     },
+  //     {
+  //       label: "内容",
+  //       type: "textarea",
+  //       value: formData.content,
+  //       onChange: (e: ChangeEventHandler) => {
+  //         onChange("content", e);
+  //       },
+  //       errors: errors.content,
+  //     },
+  //   ];
+  // }, [formData, errors]);
+
+  // /**
+  //  * 处理表单数据变动
+  //  */
+  // const onChange = useCallback(
+  //   (type, e) => {
+  //     setFormData({ ...formData, [type]: e.target.value });
+  //   },
+  //   [formData]
+  // );
+
+  // /**
+  //  * 处理数据提交
+  //  */
+  // const onSubmit = useCallback(
+  //   (e) => {
+  //     e.preventDefault();
+  //     // 发送请求前将上一次错误置空
+  //     setErrors({
+  //       title: [],
+  //       content: [],
+  //     });
+  //     Axios.post("/api/v1/poste", formData).then(
+  //       () => {
+  //         alert("发布成功！");
+  //       },
+  //       (error) => {
+  //         const response: AxiosResponse = error.response;
+  //         if (response.status === 422) {
+  //           console.log("response.data");
+  //           console.log(response.data);
+  //           setErrors({ ...response.data });
+  //         }
+  //       }
+  //     );
+  //   },
+  //   [formData]
+  // );
 
   return (
     <div>
       <h1>发布文章</h1>
-      <Form
+      {/* <Form
         fields={formFields}
         onSubmit={onSubmit}
         Buttons={
@@ -89,7 +135,8 @@ const NewPosts: NextPage = (props) => {
             <button type="submit">提交</button>
           </div>
         }
-      ></Form>
+      ></Form> */}
+      {form}
     </div>
   );
 };
