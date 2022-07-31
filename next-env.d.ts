a207d9b412068e40e574b06c443291b09a58a7aa
Andy <njloveqd@foxmail.com>
2022-07-31 16:10:48 +0800

    更新next-env.d.ts文件注释

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 
+
 declare module "*.png" {
   const value: string;
   export default value;
@@ -22,6 +23,9 @@ type FormFields = {
   errors: string[];
 };
 
+
+// 注意：一旦当我们在这个文件里有引入其他的内容那么在这个文件里声明的库都不再是全局的
+// 可通过将内容声明在其他以.d.ts结尾的文件中来解决
 import { Session } from "next-iron-session";
 import { NextApiRequest } from "next";
 interface NextIronSessionRequest extends NextApiRequest {
