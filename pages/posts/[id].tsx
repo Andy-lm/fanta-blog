4d9adec96b2a5f9520da91d46e0f6b9a7a74dc19
Andy <njloveqd@foxmail.com>
2022-08-31 17:31:54 +0800

    🎈 perf: 代码逻辑优化

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { NextPage, GetServerSideProps } from "next";
 import { getDatabaseConnection } from "lib/getDatabaseConnection";
 import { Post } from "src/entity/Post";
@@ -21,69 +21,68 @@ const postsShow: NextPage<Props> = (props) => {
   const { post } = props;
   const { data: userData } = useUser();
   const isLoggedIn = userData?.isLoggedIn;
-  const content = (
-    <div className={styles.more_actions_item}>
-      <a
-        onClick={() => {
-          Axios.post("/api/v1/post/setPost", {
-            operationType: "delete",
-            postId: post.id,
-          }).then(
-            (response) => {
-              alert("删除成功！");
-              router.push("/posts");
-            },
-            (error) => {
-              const response: AxiosResponse = error.response;
-              if (response.status === 400) {
-                console.log("response.data");
-                console.log(response.data);
-              } else if (response.status === 401) {
-                window.alert("请先登录！");
-                window.location.href = `/sign_in?return_to=${encodeURIComponent(
-                  window.location.pathname
-                )}`;
-              } else if (response.status === 400) {
-                throw new Error("删除出错！");
+
+  const content = useMemo(() => {
+    return (
+      <div className={styles.more_actions_item}>
+        <a
+          onClick={() => {
+            Axios.post("/api/v1/post/setPost", {
+              operationType: "delete",
+              postId: post.id,
+            }).then(
+              (response) => {
+                alert("删除成功！");
+                router.push("/posts");
+              },
+              (error) => {
+                const response: AxiosResponse = error.response;
+                if (response.status === 400) {
+                  console.log("response.data");
+                  console.log(response.data);
+                } else if (response.status === 401) {
+                  window.alert("请先登录！");
+                  window.location.href = `/sign_in?return_to=${encodeURIComponent(
+                    window.location.pathname
+                  )}`;
+                } else if (response.status === 400) {
+                  throw new Error("删除出错！");
+                }
               }
-            }
-          );
-        }}
-      >
-        删除文章
-      </a>
-    </div>
-  );
+            );
+          }}
+        >
+          删除文章
+        </a>
+      </div>
+    );
+  }, [post]);
+
+  const navActions = useMemo(() => {
+    return [
+      <Link href={`/posts/${post.id}/edit`}>
+        <a>
+          <Button variant="outlined" color="primary" size="small">
+            编辑
+          </Button>
+        </a>
+      </Link>,
+      <Popover content={content} trigger="hover" placement="bottomRight">
+        <div className={styles.more_actions}>
+          <MoreHorizIcon
+            style={{
+              color: "rgba(0, 0, 0, 0.55)",
+            }}
+          />
+        </div>
+      </Popover>,
+    ];
+  }, [post]);
+
   return (
     <>
-      <Nav
-        extraRight={
-          isLoggedIn
-            ? [
-                <Link href={`/posts/${post.id}/edit`}>
-                  <a>
-                    <Button variant="outlined" color="primary" size="small">
-                      编辑
-                    </Button>
-                  </a>
-                </Link>,
-                <Popover
-                  content={content}
-                  trigger="hover"
-                  placement="bottomRight"
-                >
-                  <div className={styles.more_actions}>
-                    <MoreHorizIcon
-                      style={{
-                        color: "rgba(0, 0, 0, 0.55)",
-                      }}
-                    />
-                  </div>
-                </Popover>,
-              ]
-            : []
-        }
-      ></Nav>
+      <Nav extraRight={isLoggedIn ? navActions : []}></Nav>
+      {/* 这里是富文本编辑器指定类名 */}
       <div className="typo">
         <div className={styles.main}>
           <h1>{post?.title}</h1>
