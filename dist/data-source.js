f3ddcb7f10806d68d8d99ca52950ebe5dc5e1e19
Andy <njloveqd@foxmail.com>
2022-09-28 09:29:12 +0800

    项目部署

--- dist/data-source.js
+++ dist/data-source.js
@@ -9,15 +9,16 @@ require("reflect-metadata");
 
 var _typeorm = require("typeorm");
 
+var NODE_ENV = process.env.NODE_ENV;
 var AppDataSource = new _typeorm.DataSource({
   type: "postgres",
-  // host: NODE_ENV === "development" ? "localhost" : "172.17.0.2",
-  host: "localhost",
+  host: NODE_ENV === "development" ? "localhost" : "172.17.0.2",
+  // host:"localhost",
   port: 5432,
   username: "blog",
   password: "123456",
-  // database: NODE_ENV === "development" ? "blog_development" : "blog_production",
-  database: "blog_development",
+  database: NODE_ENV === "development" ? "blog_development" : "blog_production",
+  // database:"blog_development",
   synchronize: false,
   // 该值设置为false，避免我们在修改数据的时候导致一些数据被删除
   logging: false,
